de9c0ddd1f62eba33021fb277967037d71e9ad8c
Nicolas <nicolasboulenc@gmail.com>
2022-11-28 22:09:01 +0000

    yet another commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// suits can be s(pade), c(lubs), d(iamonds), h(earts)
+// ranks can be 6 to t(en), a(ce), k(ing), q(ueen), j(ack)
+
+
+let card = {
+    suit: "",
+    rank: ""
+}
+
+
+let stacks = [
+    [ "as", "th", "ks", "qc" ],
+    [ "ah", "jd", "8s", "6h" ],
+    [ "9h", "qh", "tc", "kd" ],
+    [ "td", "ts", "jc", "7c" ],
+    [ "kc", "qd", "6d", "qs" ],
+    [ "ac", "8c", "9c", "jh" ],
+    [ "9d", "7h", "7d", "8h" ],
+    [ "kh", "9s", "js", "8d" ],
+    [ "6s", "ad", "6c", "7s" ]
+]
+
+
+function find_possible_moves() {
+
+}
+
