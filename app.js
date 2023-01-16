8c627b4780f345e9f979eb1bcdd9ba22e69389c3
Nicolas B <nicolasboulenc@gmail.com>
2023-01-16 20:06:45 +0000

    yet another commit

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ const app = {
 	y: 0,
 	off_x: 0,
 	off_y: 0,
-	stack: null
+	is_transition: false,
+	stack_from: null,
+	stack_to: null
 }
 
 
@@ -48,6 +50,7 @@ function display(stacks, game) {
 			const div = document.createElement("div");
 			div.dataset["card"] = `${rank_map[card.rank]}${card.suit}`;
 			div.addEventListener('mousedown', card_onmousedown);
+			div.addEventListener('transitionend', card_ontransitionend);
 			div.ondragstart = function() { return false; };
 			stacks[index].appendChild(div);
 		}
@@ -61,6 +64,20 @@ function card_onmousedown(evt) {
 	app.card = evt.currentTarget;
 	app.off_x = evt.offsetX;
 	app.off_y = evt.offsetY;
+	app.stack_from = app.card.parentElement;
+}
+
+
+function card_ontransitionend(evt) {
+
+	app.is_transition = false;
+	app.card.removeEventListener('transitionend', card_ontransitionend);
+	app.card.classList.remove('moving');
+	document.body.appendChild(document.getElementById('blank-card'));
+	app.stack_to.appendChild(app.card);
+
+	app.card = null;
+	app.stack_to = null;
 }
 
 
@@ -71,23 +88,29 @@ function window_onmouseup(evt) {
 	app.card.hidden = false;
 
 	if(elemBelow.classList.contains('stack') === true) {
-		app.stack = elemBelow;
+		app.stack_to = elemBelow;
 	}
 	else if(elemBelow.parentElement.classList.contains('stack') === true) {
-		app.stack = elemBelow.parentElement;
+		app.stack_to = elemBelow.parentElement;
 	}
 
-	app.card.classList.remove('moving');
-	app.stack.appendChild(app.card);
+	console.log(app);
 
-	app.card = null;
-	app.stack = null;
+	const blank = document.getElementById('blank-card');
+	app.stack_to.appendChild(blank);
+
+	app.is_transition = true;
+	app.card.addEventListener('transitionend', card_ontransitionend);
+	app.card.classList.add('transit');
+	const bbox = blank.getBoundingClientRect();
+	app.card.style.top = Math.floor(bbox.top) + 'px';
+	app.card.style.left = Math.floor(bbox.left) + 'px';
 }
 
 
 function window_onmousemove(evt) {
 
-	if(app.card !== null) {
+	if(app.card !== null && app.is_transition === false) {
 		app.card.classList.add('moving');
 		app.card.style.left = `${evt.clientX - app.off_x}px`;
 		app.card.style.top = `${evt.clientY - app.off_y}px`;
